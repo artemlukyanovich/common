f2e0ad3052913c5d98bfc1a145859acb7bef3d38
artemlukyanovich <artemlukyanovich@gmail.com>
2019-10-12 17:45:05 +0300

    12.10.2019 17:44, close to completion

--- tests_practice/python_functions.py
+++ tests_practice/python_functions.py
@@ -1,6 +1,7 @@
 import math
 import string
 
+
 def fun1(a: list, b: list):
     """Take two lists, say for example these two:
   a =[1,1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
@@ -9,16 +10,19 @@ and write a program that returns a list that contains only the elements that are
 common between the lists (without duplicates). """
     return list(set(a) & set(b))
 
+
 def fun2(string: str):
     """Return the number of times that the letter “a”
     appears anywhere in the given string"""
     return string.count("a")
+
 
 def fun3(n):
     """Write a Python program to check if a given positive
     integer is a power of three"""
     x = math.log(n, 3)
     return int(x) == x
+
 
 def fun4(n: int):
     """Write a Python program to add the digits of a
@@ -27,6 +31,7 @@ def fun4(n: int):
         n = sum(int(x) for x in str(n))
     return n
 
+
 def fun5(l: list):
     """Write a Python program to push all zeros to the end of a list."""
     c = [x for x in l if x != 0]
@@ -34,15 +39,17 @@ def fun5(l: list):
         c.append(0)
     return c
 
+
 def fun6(l: list):
     """Write a Python program to check a sequence of numbers is
     an arithmetic progression or not."""
     a = l[1] - l[0]
     for i in range(2, len(l)):
-        if l[i] - l[i-1] != a:
+        if l[i] - l[i - 1] != a:
             return False
     else:
         return True
+
 
 def fun7(l: list):
     """Write a Python program to find the number in a
@@ -53,13 +60,15 @@ def fun7(l: list):
             nums.append(i)
     return nums
 
+
 def fun8(l: list):
     """Write a Python program to find a missing number from a list."""
     nums = []
-    for i in range(l[0], l[len(l)-1]):
+    for i in range(l[0], l[len(l) - 1]):
         if i not in l:
             nums.append(i)
     return nums
+
 
 def fun9(l: list):
     """Write a Python program to count the elements
@@ -71,17 +80,20 @@ def fun9(l: list):
         n += 1
     return n
 
+
 def fun10(s: str):
     """Write a program that will take the str parameter being
     passed and return the string in reversed order."""
     return s[::-1]
+
 
 def fun11(n: int):
     """Write a program that will take the num parameter being
     passed and return the number of hours and minutes the parameter
     converts to (ie. if num = 63 then the output should be 1:3).
     Separate the number of hours and minutes with a colon."""
-    return str(n//60) + ":" + str(n%60)
+    return str(n // 60) + ":" + str(n % 60)
+
 
 def fun12(s: str):
     """Write a program that will take the parameter being passed and
@@ -91,3 +103,63 @@ def fun12(s: str):
     s2 = "".join(l for l in s if l not in string.punctuation)
     return max(s2.split(), key=len)
 
+
+def fun13():
+    s = input("Print some words: ")
+    l = s.split()
+    l.reverse()
+    return " ".join(l)
+
+
+def fun14():
+    # n = list(range(int(input("Print the number: "))+1))
+    # fib = [x for x in range(n)]
+
+    n = list(range(2, int(input("Print the number: ")) + 1))
+    base = [0, 1]
+    for i in n:
+        base.append(base[i - 1] + base[i - 2])
+    return base[1:]
+
+
+def fun15(l: list):
+    """Let’s say I give you a list saved in a variable:
+    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+    Write one line of Python that takes this list a and makes
+    a new list that has only the even elements of this list in it."""
+    return [x for x in l if x % 2 == 0]
+
+
+def fun16(n):
+    """Write a program that will add up all the numbers from
+    1 to input number. For example: if the input is 4 then your program
+    should return 10 because 1 + 2 + 3 + 4 = 10"""
+    if n >= 0:
+        return sum(range(n + 1))
+    else:
+        return sum(range(n, 0))
+
+
+def fun17(n):
+    """Write a program that will take the parameter being passed
+    and return the factorial of it."""
+    if n == 1:
+        return 1
+    else:
+        return n * fun17(n - 1)
+
+
+def fun18(s: str):
+    """Write a program that will take the str parameter being passed
+    and modify it using the following algorithm. Replace every letter
+    in the string with the letter following it in the alphabet
+    (ie. cbecomes d, zbecomes a). Then capitalize every vowel in this
+    new string (a, e, i, o, u) and finally return this modified string."""
+    vowels = ["a", "e", "i", "o", "u"]
+    s2 = [string.ascii_lowercase[str(string.ascii_lowercase).index(x)+1] if x != "z" else "a" for x in s]
+    res = "".join([x.upper() if x in vowels else x for x in s2])
+    return res
+
+print(fun18("abcdefghz"))
+
+# print("".join(x if x != "c" else "C" for x in ["a", "b", "c"] ))
--- tests_practice/python_tests.py
+++ tests_practice/python_tests.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import patch
 from python_functions import fun1, fun2, fun3, fun4, fun5, fun6, \
-    fun7, fun8, fun9, fun10, fun11, fun12
+    fun7, fun8, fun9, fun10, fun11, fun12, fun13, fun14, fun15, \
+    fun16, fun17, fun18
 
 class Tests(unittest.TestCase):
     def test_fun1(self):
@@ -42,4 +44,25 @@ class Tests(unittest.TestCase):
 
     def test_fun12(self):
         self.assertEqual(fun12("fun&!! time"), "time")
+
+    @patch('builtins.input', return_value="My name is Michele")
+    def test_fun13(self, mock):
+        self.assertEqual(fun13(), "Michele is name My")
+
+    @patch('builtins.input', return_value=10)
+    def test_fun14(self, mock):
+        self.assertEqual(fun14(), [1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+
+    def test_fun15(self):
+        self.assertEqual(fun15([1, 4, 9, 16, 25, 36, 49, 64, 81, 100]), [4, 16, 36, 64, 100])
+
+    def test_fun16(self):
+        self.assertEqual(fun16(5), 15)
+        self.assertEqual(fun16(-5), -15)
+
+    def test_fun17(self):
+        self.assertEqual(fun17(5), 120)
+
+    def test_fun18(self):
+        self.assertEqual(fun18("abcdefghz"), "bcdEfghIA")
 
