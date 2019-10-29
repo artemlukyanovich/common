9c7afa390f0e8b1e38b711242af351098f200ab8
artemlukyanovich <artemlukyanovich@gmail.com>
2019-10-30 00:02:44 +0200

    flask intro hw

---
+++ flask_intro_hw/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_home_page():
+    return render_template('home.html')
+
+@app.route('/vegetables')
+def get_vegetables_page():
+    return render_template('vegetables.html', vg_list=['beans', 'carrot', 'beetroot', 'cucumber'])
+
+@app.route('/fruits')
+def get_fruits_page():
+    return render_template('fruits.html', fr_list=['melon', 'apple', 'strawberry', 'grape'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
